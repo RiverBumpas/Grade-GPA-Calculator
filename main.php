195813e87da6829045bd7dd6cee957d2c26fd41b
RiverBumpas <riverb802@gmail.com>
2024-03-18 15:46:36 -0400

    creating basic html file

---
+++ main.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="River Bumpas and Surya Malik">
+        <title>Final Grade/ GPA Calculator</title>
+        <meta name="description" content="This page is for calculating a students final grade in a course and overall GPA.">
+        <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+    </head>
+    <body>
+    	<header>
+    		<h1>Final Grade and GPA Calculator</h1>
+            
+    	</header>
+
+        <main>
+            <h2> Final Grade Calculator </h2>
+            <p> content </p>
+
+            <h2> GPA Calculator </h2>
+            <p> content </p>
+
+    </body>
+</html>
