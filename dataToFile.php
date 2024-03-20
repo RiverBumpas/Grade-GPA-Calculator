752250d1b1cce917711a5f6439f1aad3f6c61ae1
RiverBumpas <riverb802@gmail.com>
2024-03-20 11:32:14 -0400

    fixed file appending

--- dataToFile.php
+++ dataToFile.php
@@ -12,7 +12,7 @@ $file = 'results.txt';
 
 file_put_contents($file, '');
 
-if (file_put_contents($file, $data, FILE_APPEND | LOCK_EX) !== false) {
+if (file_put_contents($file, $data, FILE_APPEND) !== false) {
     echo 'Data has been saved successfully.';
 } else {
     echo 'There was an error saving your data.';
