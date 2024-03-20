22df344aac544065e62057f37c13b2af6260e8bb
smalik1145 <97012367+smalik1145@users.noreply.github.com>
2024-03-20 19:27:55 -0400

    fixed css issues

--- finalGrade.js
+++ finalGrade.js
@@ -12,7 +12,7 @@ function addCourse() {
             <legend>Course ${additionalCourses}</legend>
             <input type="text" name="courseName${additionalCourses}" placeholder="Course Name" required>
             <div class="totalCategories"></div>
-            <button type="button" id="addCategory" onclick="addCategory(this.parentNode)">Add Category</button>
+            <button type="button" class="addCategory" onclick="addCategory(this.parentNode)">Add Category</button>
             <button type="button" class="remove" onclick="removeElement(this.parentNode.parentNode)">Remove Course</button>
         </fieldset>
     `; // sets html for new course div, input field for course name, sets a div for categories which will also have dynamically added fields, creates button for these fields
@@ -30,10 +30,9 @@ function addCategory(courseDiv) { // same logic as addCourse essentially
             <input type="text" name="${courseDiv.id}categoryName${additionalCategories}" placeholder="Category Name" required>
             <input type="number" name="${courseDiv.id}categoryWeight${additionalCategories}" placeholder="Weight (decimal)" required min="0" max="1" step="0.01">
             <div class="totalAssignments"></div>
-            <button type="button" id="addAssignment" onclick="addAssignment(this.parentNode)">Add Assignment</button>
+            <button type="button" class="addAssignment" onclick="addAssignment(this.parentNode)">Add Assignment</button>
             <button type="button" class="remove" onclick="removeElement(this.parentNode.parentNode)">Remove Category</button>
         </fieldset>
-        
     `;
     totalCategories.appendChild(categoryDiv);
 }
