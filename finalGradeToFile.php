6fbe8b228e4bef43f32c649589dce82ec2e0621e
RiverBumpas <riverb802@gmail.com>
2024-03-21 21:19:49 -0400

    changes

--- finalGradeToFile.php
+++ finalGradeToFile.php
@@ -1,14 +1,16 @@
 <?php
 
-$courseName = $_POST['courseName1'] ?? '';
-$categoryName = $_POST['course1categoryName1'] ?? '';
-$categoryWeight = $_POST['course1categoryWeight1'] ?? '';
-$assignmentName = $_POST['course1assignmentName1'] ?? '';
-$assignmentScore = $_POST['course1assignmentScore1'] ?? '';
 
-$data = "Name: $courseName\nCategory Name: $categoryName\nCategory Weight: $categoryWeight\nAssignment Name: $assignmentName\nAssignment Score: $assignmentScore";
+$data = "finalGrade . \n";
 
 $file = 'results.txt';
+
+//foreach($_POST as $course){
+//    $data .= $course + '\n' + $course[0][0];
+
+//}
+
+print_r($_POST);
 
 file_put_contents($file, '');
 
