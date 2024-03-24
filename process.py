cb24e546d87cd154d24e71e8a006be67ab96bc52
RiverBumpas <riverb802@gmail.com>
2024-03-24 11:42:49 -0400

    work on final grade processing

--- process.py
+++ process.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
 
     with open("results.txt", "r") as file:
         path_choice = file.readline().strip()
+        
         if path_choice == "gpaCalculator":
             for line in file:
                 info = line.strip().split(',')
@@ -15,9 +16,34 @@ if __name__ == "__main__":
                 gpa_data.append((course, credits, grade))
             #run cpp file
 
-        #elif path_choice == "finalGrade":
-            #collect variables for finalGrade, run finalGrade c++ file
-            
+        elif path_choice == "finalGrade":
+                #NOT WORKING YET, NEEDS MORE WORK
+                lines = file.readlines()  
 
+                course_name = ""
+                categories = []
+                category_data = []
+
+                for line in lines:
+                    line = line.strip() 
+
+                    # If the line is empty, go to next course
+                    if not line:
+                        course_name = ""
+                        categories = []
+                        category_data = []
+                        continue
+
+                    if not course_name:
+                        course_name = line
+                        continue
+
+                    category_info = line.split(',')
+                    category_name = category_info[0]
+                    category_weight = float(category_info[1])
+                    assignments = [(category_info[i], int(category_info[i+1])) for i in range(2, len(category_info), 2)]
+
+                    categories.append([category_name, category_weight, assignments])
+                    #run cpp file
+    print(final_grade_data)
         
-    
