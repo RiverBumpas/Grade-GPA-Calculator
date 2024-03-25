be140a5f89856cfac93b10b04900b91990babfa9
smalik1145 <97012367+smalik1145@users.noreply.github.com>
2024-03-24 22:14:02 -0400

    python changes

--- process.py
+++ process.py
@@ -1,29 +1,23 @@
 import sys
-import json
-import subprocess
 
 def handle(file_path):
-    gpa_data = []
     with open(file_path, "r") as file:
         path_choice = file.readline().strip()
         
         if path_choice == "gpaCalculator":
+            gpa_data = []
             for line in file:
                 info = line.strip().split(',')
                 course = info[0]
                 credits = int(info[1])
                 grade = int(info[2])
                 gpa_data.append((course, credits, grade))
-            with open("data.json", "w") as f:
-                json.dump(gpa_data, f)
-            #run cpp file
 
         elif path_choice == "finalGrade":
             lines = file.readlines()
 
             course_name = ""
             courses = []
-            category_data = []
             assignments = []
 
             for line in lines:
@@ -32,7 +26,6 @@ def handle(file_path):
                 # If the line is empty, go to next course
                 if not line:
                     course_name = ""
-                    category_data = []
                     assignments = []
                     continue
 
@@ -46,10 +39,7 @@ def handle(file_path):
                 assignments = [(category_info[i], int(category_info[i+1])) for i in range(2, len(category_info)-1, 2)]
 
                 courses.append([course_name, [category_name, category_weight, assignments]])
-            with open("data.json", "w") as f:
-                json.dump(courses, f)
-            #run cpp file
-
+    return result
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python3 process.py <file_path>")
